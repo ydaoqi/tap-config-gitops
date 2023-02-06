81b6e0bf0259609e12bdc70206ba24450c88022e
Daoqi Yang <ydaoqi@vmware.com>
2023-02-06 15:25:13 -0500

    first commit

---
+++ java-server-side-ui/build.gradle.kts
@@ -0,0 +1,34 @@
+
+plugins {
+    id("org.springframework.boot") version "2.7.5"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("java")
+}
+
+group = "com.vmware.tap.accelerators"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+extra["snakeyaml.version"] = "1.32"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
